3a3bbdcda860d8489fc5169753f5ca9a1f47dae2
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-02-17 00:07:01 -0600

    adds pta for different n values

--- image_classification.py
+++ image_classification.py
@@ -1,5 +1,6 @@
 import struct as st
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(100)
 
@@ -18,11 +19,8 @@ def step_activation(local_induced_field):
     return temp
 
 
-def pta():
+def pta(n, eta, epsilon):
     
-    learning_rate = 0.5
-    threshold = 0.15
-    n = 60000
     W = np.random.uniform(-1, 1, size=(10, 784))
     epoch = 0
     errors = []
@@ -38,25 +36,43 @@ def pta():
             d_X = np.eye(10, 1, train_y[i]*-1)
             WX = np.dot(W, X)
             u_WX = step_activation(WX.reshape((10, )))
-            W = W + learning_rate*np.dot((d_X - u_WX), X.T)
-        if errors[epoch-1] / n <= threshold:
+            W = W + eta*np.dot((d_X - u_WX), X.T)
+        if errors[epoch-1] / n <= epsilon:
             break
+        elif epoch >= 50:
+            return -1
+        
+    plt.plot(range(epoch), errors)
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Number of misclassifications")
+    plt.show()
     return W
 
 
-def test_classification(W):
+def test_classification(W, n):
     
     test_errors = 0
-    for i in range(np.size(test_X, 0)):
+    no_rows = np.size(test_X, 0)
+    for i in range(no_rows):
         if np.dot(W, test_X[i].reshape((784, 1))).argmax() != test_y[i]:
             test_errors += 1
-    print(test_errors)
+    print(f"Percentage of error on test dataset are (for n = {n}):", (test_errors/no_rows)*100)
 
 
 if __name__ == "__main__":
+    
     train_X = read_idx('train-images.idx3-ubyte')
     train_y = read_idx('train-labels.idx1-ubyte')
     test_X = read_idx('t10k-images.idx3-ubyte')
     test_y = read_idx('t10k-labels.idx1-ubyte')
-    W = pta()
-    test_classification(W)
+    W = pta(30000, 0.5, 0.15)
+    test_classification(W, 30000)
+    W = pta(50, 1, 0)
+    test_classification(W, 50)
+    W = pta(1000, 1, 0)
+    test_classification(W, 1000)
+    W = pta(60000, 1, 0)
+    if W != -1:
+        test_classification(W, 60000)
+    else:
+        print("DIDN'T CONVERGE")
